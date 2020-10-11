02242ddb87049678f7ca058bb2a7041f03e9e3f0
asciishell <asciishell@yandex.ru>
2020-10-11 23:54:54 +0300

    cleanup code

--- lab_02/de10-nano/de10_nano.v
+++ lab_02/de10-nano/de10_nano.v
@@ -34,29 +34,6 @@ module de10_nano
     inout   [35:0]  GPIO_1
 );
 
-    // wires & inputs
-    wire          clk;
-    wire          clkIn     =  FPGA_CLK1_50;
-    wire          rst_n     =  KEY[0];
-    wire          clkEnable =  ~KEY[1];
-    wire [  3:0 ] clkDevide =  4'b1000;
-    wire [  4:0 ] regAddr   =  { 1'b0, SW [3:0] };
-    wire [ 31:0 ] regData;
-
-    //cores
-    sm_top sm_top
-    (
-        .clkIn      ( clkIn     ),
-        .rst_n      ( rst_n     ),
-        .clkDevide  ( clkDevide ),
-        .clkEnable  ( clkEnable ),
-        .clk        ( clk       ),
-        .regAddr    ( regAddr   ),
-        .regData    ( regData   )
-    );
-
-    //outputs
-    assign LED[0]   = clk;
-    assign LED[7:1] = regData[6:0];
+    layer layer (GPIO_0[19:0], LED[3:0]);    
 
 endmodule
